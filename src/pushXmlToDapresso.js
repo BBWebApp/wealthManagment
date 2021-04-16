493ab6fcc749896d5643937456c23589012f92e4
Mahmoud Elgohary <mahmoud.elgohary@hotmail.com>
2021-04-16 13:15:17 +0200

    first from new laptop

--- src/pushXmlToDapresso.js
+++ src/pushXmlToDapresso.js
@@ -1,38 +1,26 @@
 var parseString = require("xml2js").parseString;
-var axios = require("axios")
-var base = require("base-64")
-var fs = require("fs")
+var axios = require("axios");
+var base = require("base-64");
+var fs = require("fs");
 
-
-var url2 = "http://localhost:8011/proxy/workflow/3/task/1/upload"
+var url2 = "http://localhost:8020/proxy/workflow/3/task/1/upload";
 var tok = "gui_client:kFjfAh68k$$ADUjPr?vPA";
 var hash = base.encode(tok);
 var Basic = "Basic " + hash;
-var xmlFileUpload = fs.readFileSync("D:/VisualStudioWorkspace/NodeJS_stuff/WM-Prototype/wealthmanagment/src/xmlStructure.xml", {encoding: 'utf-8'})
-var datas = "d";
-var columns;
-var link;
+var xmlFileUpload = fs.readFileSync(
+  "D:/VisualStudioWorkspace/wealthManagment/src/xmlStructure.xml",
+  { encoding: "utf-8" }
+);
 function name() {
-
-    axios
-        .post(
-            url2, xmlFileUpload,
-            {
-                headers: {
-                    Authorization: Basic,
-                },
-            }
-        )
-        .then((response) => {
-
-
-
-            console.log(response.data)
-
-        });
+  axios
+    .post(url2, xmlFileUpload, {
+      headers: {
+        Authorization: Basic,
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    });
 }
 
-
 XXX = name();
-
-
