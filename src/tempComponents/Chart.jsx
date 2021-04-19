bce9494bf45f78a8f3713a8179e8b4698c9318e2
MahmoudElgohary1992 <mahmoud.elgohary@bbconsult.de>
2021-04-19 13:40:57 +0200

    npm bit packages

---
+++ src/tempComponents/Chart.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({});
+
+const Chart = () => {
+  return (
+    <Typography align="center" variant="h2" component="h2">
+      Chart
+    </Typography>
+  );
+};
+
+export default Chart;
---
+++ src/tempComponents/Header.jsx
@@ -0,0 +1,87 @@
+import { Hidden } from "@material-ui/core";
+import AppBar from "@material-ui/core/AppBar";
+import IconButton from "@material-ui/core/IconButton";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import React from "react";
+import { withRouter } from "react-router-dom";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+  },
+}));
+
+const Header = (props) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const { history } = props;
+  const theme = useTheme();
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClicked = (e) => {
+    history.push(e);
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            Photos
+          </Typography>
+
+          <div>
+            <Hidden xsDown>
+              <IconButton
+                className={classes.menuButton}
+                color="inherit"
+                onClick={handleMenu}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Hidden>
+
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={open}
+              onClose={() => setAnchorEl(null)}
+            >
+              <MenuItem onClick={() => handleClicked('/')}>Home</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default withRouter(Header);
